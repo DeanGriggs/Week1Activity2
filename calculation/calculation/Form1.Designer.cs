848bb550cee07f2bd95f618c8672b4537eea2c6e
DeanGriggs <117603385+DeanGriggs@users.noreply.github.com>
2022-11-09 20:05:48 -0700

    Add files via upload

---
+++ calculation/calculation/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace calculation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbNumber2 = new System.Windows.Forms.TextBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.tbNumber = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Please enter your favorite number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Your favorite number plus 2";
+            // 
+            // tbNumber2
+            // 
+            this.tbNumber2.Location = new System.Drawing.Point(207, 170);
+            this.tbNumber2.Name = "tbNumber2";
+            this.tbNumber2.Size = new System.Drawing.Size(100, 20);
+            this.tbNumber2.TabIndex = 2;
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(232, 112);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 23);
+            this.btnRun.TabIndex = 4;
+            this.btnRun.Text = "Run It.";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // tbNumber
+            // 
+            this.tbNumber.Location = new System.Drawing.Point(207, 61);
+            this.tbNumber.Name = "tbNumber";
+            this.tbNumber.Size = new System.Drawing.Size(100, 20);
+            this.tbNumber.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 251);
+            this.Controls.Add(this.tbNumber);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.tbNumber2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximumSize = new System.Drawing.Size(400, 290);
+            this.MinimumSize = new System.Drawing.Size(400, 290);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbNumber2;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.TextBox tbNumber;
+    }
+}
+
---
+++ calculation/calculation/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+           String f = tbNumber.Text;
+            Console.WriteLine(f.GetType());
+            int r=Int32.Parse(f);
+
+            tbNumber2.Text = "" + (r + 2);
+        }
+
+       
+    }
+    }
+
